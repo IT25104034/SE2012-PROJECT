daabdb0aaca5ddb91f351f395798af70291fb9d8
Vhikaash <it25102797@my.sliit.lk>
2026-09-26 16:09:44 +0530

    feat: add order domain model

---
+++ backend/src/main/java/com/hardwarestore/hardwarestore/model/Order.java
@@ -0,0 +1,66 @@
+package com.hardwarestore.hardwarestore.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User customer;
+
+    @Column(nullable = false)
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+
+    public Order() {
+    }
+
+    public Order(User customer, LocalDateTime orderDate, OrderStatus status) {
+        this.customer = customer;
+        this.orderDate = orderDate;
+        this.status = status;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public User getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(User customer) {
+        this.customer = customer;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+}
